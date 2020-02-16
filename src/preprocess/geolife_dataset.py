5b09c5776be6345f51d64eb7f4891d6c63fba200
Yinglai Yang <yinglai.yang@gmail.com>
2020-02-16 09:42:03 +0100

    Fixes error.

--- src/preprocess/geolife_dataset.py
+++ src/preprocess/geolife_dataset.py
@@ -11,6 +11,9 @@ class GeoLifeDataSet:
 
     def load_data(self):
         """ Loads references to all required data (recursively top-down) for further processing.
+
+            For now it will assume the simple structure and fixed names as described in the data 
+            manual of the GeoLife dataset.
         """
         with os.scandir(self.path) as dir_iter:
             pot_participants = (entry for entry in dir_iter if entry.is_dir(follow_symlinks = False))
@@ -19,7 +22,7 @@ class GeoLifeDataSet:
                 pot_trajectories_path = os.path.join(entry.path, 'Trajectories')
 
                 if os.path.isdir( pot_trajectories_path ):
-                    participants.append( GeoLifeParticipant(pot_trajectories_path) )
+                    self.participants.append( GeoLifeParticipant(pot_trajectories_path) )
 
 class GeoLifeParticipant:
     id_key = 999999
