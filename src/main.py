5bf8ab61c931de08e2d15c5a3902e0e6cdf0543f
Yinglai Yang <yinglai.yang@gmail.com>
2020-04-16 19:00:37 +0200

    Some updates on the way to Parquet Basic.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import preprocess.geolife_dataset as gldata
 import json
+import os
 
 if __name__ == '__main__':
     # Load JSON config
@@ -9,6 +10,24 @@ if __name__ == '__main__':
         config = json.load(f)
 
     # Crawl through dataset
-    dataset = gldata.GeoLifeDataSet(config['GeoLife_Specifics']['dataset_path'])
+    # =====================
+
+    # Setup the crawler first.
+    # ------------------------
+    dataset = gldata.GeoLifeDataSet(config['GeoLife_Specifics']['dataset_root_path'])
 
     dataset.load_data_references()
+
+    print(dataset.participants[0].trajectories[0].file_path)
+    print( os.path.relpath(dataset.participants[0].trajectories[0].file_path, start=config['GeoLife_Specifics']['dataset_root_path']) )
+
+    # Step through all data to transform to parquet basic.
+    # ----------------------------------------------------
+    for participant in dataset.participants:
+        for trj in participant.trajectories:
+            trj.load_data(config)
+
+            # Define the target file path for the converted format:
+            #target_file_path = format(config['GeoLife_Specifics']['converted_dataset_path']).
+
+            trj.unbind_data()
