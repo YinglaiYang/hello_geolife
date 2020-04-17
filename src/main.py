482a4c211dfc623a018e2f7e74540cb102fb988d
Yinglai Yang <yinglai.yang@gmail.com>
2020-04-17 20:03:57 +0200

    Savegame

--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ import preprocess.geolife_dataset as gldata
 import json
 import os
 
+# The following import is only for performance measurement during development
+from time import perf_counter
+
+# The following imports will need to be capsuled into the target formats.
+import pandas as pd
+
 if __name__ == '__main__':
     # Load JSON config
     config = {}
@@ -23,11 +29,31 @@ if __name__ == '__main__':
 
     # Step through all data to transform to parquet basic.
     # ----------------------------------------------------
+    timer_start = perf_counter()
+
     for participant in dataset.participants:
         for trj in participant.trajectories:
-            trj.load_data(config)
-
             # Define the target file path for the converted format:
-            #target_file_path = format(config['GeoLife_Specifics']['converted_dataset_path']).
+            convert_root_path = config['converted_dataset_root_paths']['parquet_basic']
 
-            trj.unbind_data()
+            file_relative_path          = os.path.relpath( trj.file_path, start=config['GeoLife_Specifics']['dataset_root_path'] )
+            file_relative_path_nameonly = os.path.splitext( file_relative_path )[0]
+            
+            target_file_path = os.path.join( convert_root_path, file_relative_path_nameonly + '.parquet' )
+
+            # Create the file path directories if they do not exist
+            target_dir_path = os.path.dirname(target_file_path)
+
+            if not os.path.exists( target_dir_path ):
+                os.makedirs( target_dir_path )
+
+            # Get the data into a dataframe and save it in the target file path as a parquet file.
+            trj_df = trj.get_data(config)
+            trj_df.to_parquet( target_file_path )  
+
+        print('=> Finished conversion to Parquet Basic for participant {}'.format(participant.id_key))
+
+    timer_end = perf_counter()
+
+    print('===============')
+    print('The complete conversion took {}s.'.format(timer_end - timer_start))
